34df68231c19414ba432f3d189b8ef94a49d08c2
k123s456h <k123s456h@gmail.com>
2022-08-21 23:27:26 +0900

    8^8

--- inject/index.js
+++ inject/index.js
@@ -22,8 +22,10 @@ const titleObserverOptions = {
   subtree: false,
 }
 
+let isVod, isPopout;
 let dccons;
 let runner;
+let rightColumn;
 let inputArea, inputAreaParent, inputAreaContent;
 let chatArea, chatObserver;
 let iconArea;
@@ -33,8 +35,15 @@ let titleArea, titleObserver;
 
 /***************************************************** */
 
-const logger = (...args) => {
-  console.log("[DCCON Selector] ", ...args);
+const logger = (level=1, ...args) => {
+  if(level === 1)
+  {
+    console.log("[DCCON Selector] ", ...args);
+  }
+  else if(level === 2)
+  {
+    console.debug("[DCCON Selector] ", ...args);
+  }
 }
 
 const getStreamerFromURL = () => {
@@ -55,6 +64,12 @@ const getStreamerFromURL = () => {
   return streamer;
 }
 
+const getRightColumn = () => {
+  return isPopout 
+  ? document.querySelector('.chat-room')
+  : document.querySelector('.right-column');
+}
+
 const getChatInputArea = () => {
   return document.querySelector('[data-a-target="chat-input"]');
 }
@@ -68,7 +83,21 @@ const getIconArea = () => {
 }
 
 const getChatArea = () => {
-  return document.querySelectorAll(".chat-scrollable-area__message-container")[0];
+  return isVod
+  ? document.querySelector(".video-chat__message-list-wrapper ul")
+  : document.querySelector(".chat-scrollable-area__message-container");
+}
+
+const getRelativePosition = (target, mouseEvent) => {
+  const {x, y} = target.getBoundingClientRect();
+  const {clientX, clientY} = mouseEvent;
+
+  logger(2, {x: x, y: y}, {x: clientX, y: clientY}, {x: clientX-x , y: clientY-y});
+
+  return {
+    x: clientX - x,
+    y: clientY - y,
+  }
 }
 
 const dcconFilter = (keyword) => {
@@ -111,8 +140,8 @@ const toggleSelector = (open) => {
   }
   else 
   {
-    const dialogParent = document.getElementById("dccon-dialog");
-    dialogParent.innerHTML = ""
+    dcconDialog = document.getElementById('dccon-dialog');
+    dcconDialog.innerHTML = ""
     dcconSelectorRoot.classList.remove("show");
     dcconSelectorRoot.classList.add("hide");
     inputAreaParent.removeChild(dcconSelectorRoot);
@@ -120,63 +149,28 @@ const toggleSelector = (open) => {
   }
 }
 
+const dcconMouseoverHandler = async (e) => {
+  dcconDialog = document.getElementById('dccon-dialog');
+  const popup = document.createElement('div');
+  const keyword = e.target.getAttribute("data-keywords").split(",")[0];
+  const pos = getRelativePosition(dcconDialog, e);
+  popup.classList.add("dcccon-dialog-popup");
+  popup.innerText = `${keyword}`;
+  popup.style.left = `${pos.x}px`;
+  popup.style.top = `${pos.y}px`;
+  popup.style.display = `block`;
+  dcconDialog.innerHTML = "";
+  dcconDialog.appendChild(popup);
+}
+
+const dcconMouseoutHandler = async (e) => {
+  dcconDialog = document.getElementById('dccon-dialog');
+  dcconDialog.innerHTML = "";
+}
+
 const dcconClickHandler = async (e) => {
   e.preventDefault();
   const currentInput = getChatInputAreaContent(inputArea).innerText.trim().split(" ").pop();
-  
-
-  // const removeLength = currentInput.length;
-  /*
-  // hange inputArea to `textarea` tag
-  // this is dangerous. 
-  
-  // if(inputAreaContent.tagName !== "INPUT")
-  // {
-  //   logger(inputArea, inputAreaContent);
-
-  //   const customInputArea = document.createElement("input");
-  //   Array.from(inputAreaContent.attributes).forEach(attribute => {
-  //     customInputArea.setAttribute(
-  //       attribute.nodeName === "id" ? "data-id" : attribute.nodeName,
-  //       attribute.nodeValue
-  //     );
-  //   });
-  //   while(inputAreaContent.childNodes.length > 0)
-  //   {
-  //     customInputArea.appendChild(inputAreaContent.childNodes[0]);
-  //   }
-  //   customInputArea.type = "text";
-  //   customInputArea.value = inputAreaContent.innerText.trim();
-
-  //   logger(inputArea, customInputArea);
-  //   inputAreaContent.parentElement.replaceChild(customInputArea, inputAreaContent);
-  //   inputAreaContent = customInputArea;
-  // }
-
-  
-  // how to remove string in <span> tag?
-  
-  // const keyboardEvent = new KeyboardEvent("keyup", {
-  //   bubbles: true,
-  //   cancelable: false,
-  //   view: window,
-  //   ctrlKey: false,
-  //   altKey: false,
-  //   shiftKey: false,
-  //   metaKey: false,
-  //   composed: true,
-  //   key: "Backspace",
-  //   code: "Backspace",
-  // });
-  // for(let i=0; i<removeLength; i++)
-  // {
-  //   logger(inputAreaContent.innerText, inputAreaContent.innerText.slice(0, -1));
-
-  //   inputArea.dispatchEvent(keyboardEvent);
-  //   inputAreaContent.innerText = inputAreaContent.innerText.slice(0, -1);
-  //   logger(inputAreaContent.value);
-  // }
-  */
   
   inputArea.focus();
   let keywords = e.target.getAttribute("data-keywords").split(',');
@@ -190,7 +184,7 @@ const dcconClickHandler = async (e) => {
       isPrefix = true;
     }
   }
-  logger(currentInput, currentInput.length, keyword, "prefix?", isPrefix);
+  logger(2 , currentInput, currentInput.length, keyword, "prefix?", isPrefix);
   if(isPrefix)
   {
     /**
@@ -213,19 +207,49 @@ const dcconClickHandler = async (e) => {
     /**
      * 아니라면 그냥 클립보드에 복사
      */
-    const parent = document.getElementById("dccon-dialog");
-    const pos = e.target.getBoundingClientRect();
-    const popup = document.createElement('div');
-    popup.classList.add("dcccon-dialog-popup");
-    popup.innerText = `${keyword} 복사됨!`;
-    popup.style.left = `${pos.x}px`;
-    popup.style.top = `${e.pageY + 1 - pos.y - 200}px`;
-    popup.style.display = `block`;
-    parent.innerHTML = "";
-
     await navigator.clipboard.writeText(keyword);
 
-    parent.appendChild(popup);
+    dcconDialog = document.getElementById('dccon-dialog');
+    const pos = getRelativePosition(dcconDialog, e);
+    const popup = document.createElement('div');
+    popup.classList.add("dcccon-dialog-popup", "disappear");
+    popup.innerText = `${keyword} 복사됨!`;
+    popup.style.left = `${pos.x}px`;
+    popup.style.top = `${pos.y}px`;
+    popup.style.display = `block`;
+    dcconDialog.innerHTML = "";
+    dcconDialog.appendChild(popup);
+  }
+}
+
+const dcconClickHandlerInChat = async (e) => {
+  e.preventDefault();
+  const keyword = `~${e.target.getAttribute("data-keywords").split(',')[0]}`;
+
+  if(inputArea)
+  {
+    const dataTransfer = new DataTransfer();
+    dataTransfer.setData("text", `${keyword} `);
+    const event = new ClipboardEvent("paste", {
+      clipboardData: dataTransfer,
+      bubbles: true,
+    });
+    inputArea.dispatchEvent(event);
+  }
+  else 
+  {
+    await navigator.clipboard.writeText(keyword);
+
+    dcconDialog = document.getElementById('dccon-dialog');
+    const popup = document.createElement('div');
+    const pos = getRelativePosition(dcconDialog, e);
+    popup.classList.add("dcccon-dialog-popup", "disappear");
+    popup.innerText = `${keyword} 복사됨!`;
+    popup.style.left = `${pos.x}px`;
+    popup.style.top = `${pos.y}px`;
+    popup.style.display = `block`;
+    dcconDialog.innerHTML = "";
+    dcconDialog.appendChild(popup);
   }
 }
 
@@ -254,8 +278,11 @@ const chatInputHandler = (e) => {
     itemIcon.setAttribute("data-name", dccon.name);
     itemIcon.setAttribute("data-keywords", `${keywords}`);
     itemIcon.setAttribute("data-tags", dccon.tags);
-    itemIcon.title = keywords.join(" ");
+    // itemIcon.title = keywords.join(" ");
+    itemIcon.alt = keywords;
     itemIcon.onclick = dcconClickHandler;
+    itemIcon.onmouseover = dcconMouseoverHandler;
+    itemIcon.onmouseout = dcconMouseoutHandler;
 
     dcconListContainer.appendChild(itemIcon);
   }
@@ -273,42 +300,59 @@ const findExactlyMatch = (keyword) => {
 }
 
 const replaceChatData = (chatDiv) => {
+  const parent = chatDiv.parentElement;
   const text = chatDiv.innerText;
-  let newHTML = "";
-  let tokens = [];
-  for(let token of text.split(" "))
-  {
+
+  const debugList = [];
+
+  chatDiv.innerText = "";
+  text.split(" ").forEach((token, index, arr) => {
     if(token.startsWith('~'))
     {
       const keyword = token.substr(1);
       const dccon = findExactlyMatch(keyword)
       if(dccon)
       {
-        token = `<br>
-                <img 
-                  class="dccon" 
-                  src="${dccon.uri}" 
-                  data-uri="${dccon.uri}" 
-                  data-name="${dccon.name}" 
-                  data-keywords="~${keyword}" 
-                  title="~${keyword}" 
-                  data-tags="${dccon.tags}">
-                </img>
-                <br>`
+        const img = document.createElement("img");
+        img.classList.add("dccon");
+        img.src = dccon.uri;
+        // img.title = `~${keyword}`;
+        img.alt = `~${keyword}`;
+        img.setAttribute("data-uri", dccon.uri);
+        img.setAttribute("data-name", dccon.name);
+        img.setAttribute("data-keywords", dccon.keywords);
+        img.setAttribute("data-tags", dccon.tags);
+        img.onclick = dcconClickHandlerInChat;
+        img.onmouseover = dcconMouseoverHandler;
+        img.onmouseout = dcconMouseoutHandler;
+        parent.appendChild(document.createElement("br"));
+        parent.appendChild(img);
+        parent.appendChild(document.createElement("br"));
+
+        debugList.push(img);
+
+        return;
       }
     }
-    tokens.push(token);
-  }
-  newHTML = tokens.join(" ");
-  if(text !== newHTML)
-  {
-    chatDiv.innerHTML = newHTML
-    // logger(`${text} => ${newHTML}`);
-  }
+
+    const txt = document.createElement("span");
+    txt.classList.add("text-fragment");
+    txt.setAttribute("data-a-target", "chat-message-text");
+    txt.innerText = token;
+    if(index < arr.length - 1)
+    {
+      txt.innerText += " ";
+    }
+    parent.appendChild(txt);
+
+    debugList.push(txt);
+  });
+
+  logger(2, text, '=>', debugList.map(v => v.alt || v.innerText));
 }
 
 const replaceChatAll = () => {
-  chatArea.querySelectorAll(".text-fragment").forEach(chatDiv => {
+  chatArea && chatArea.querySelectorAll(".text-fragment").forEach(chatDiv => {
     replaceChatData(chatDiv);
   });
 }
@@ -319,15 +363,16 @@ const chatObserverHandler = (mutationList, observer) => {
     const children = record.addedNodes;
     for(const child of children)
     {
-      const chatDiv = child.querySelector(".text-fragment");
-      if(!chatDiv) continue;
-      replaceChatData(chatDiv);
+      child.querySelectorAll(".text-fragment").forEach(chatDiv => {
+        replaceChatData(chatDiv);
+      });
     }
   }
 }
 
+
 const titleObserverHandler = (mutationList, observer) => {
-  logger(`title changed! reloading...`);
+  logger(2 , `title changed! reloading...`);
   inputAreaParent = undefined;
   dcconSelectorRoot = undefined;
   dcconListContainer = undefined;
@@ -338,32 +383,15 @@ const titleObserverHandler = (mutationList, observer) => {
   inputAreaParent = undefined;
   iconArea = undefined;
   chatArea = undefined;
-  chatObserver.disconnect();
+  chatObserver && chatObserver.disconnect();
   chatObserver = undefined;
+  isVod = location.href.indexOf("videos") !== -1;
+  isPopout = location.href.indexOf("popout") !== -1;
 
-  while(!inputArea)
-  {
-    inputArea = getChatInputArea();
-    if(inputArea)
-    {
-      inputAreaExists();
-    }
-  }
-  while(!iconArea)
-  {
-    iconArea = getIconArea();
-  }
-  while(!chatArea)
-  {
-    chatArea = getChatArea();
-    if(chatArea)
-    {
-      replaceChatAll();
-      chatObserver = new MutationObserver(chatObserverHandler);
-      chatObserver.observe(chatArea, chatObserverOptions);
-    }
-  }
-  logger(`reloaded!`);
+  logger(2 , "vod?", isVod);
+
+  elementInitializer();
+  logger(2 , `reloaded!`);
 }
 
 
@@ -385,17 +413,13 @@ const inputAreaExists = () => {
     dcconSelectorRoot = document.createElement("div");
     dcconSelectorWrapper = document.createElement("div");
     dcconListContainer = document.createElement("div");
-    dcconDialog = document.createElement('div');
-    dcconDialog.id = "dccon-dialog";
 
     dcconSelectorRoot.classList.add("dccon-selector-root");
     dcconSelectorWrapper.classList.add("dccon-selector-wrapper");
     dcconListContainer.classList.add("dccon-list");
 
     dcconSelectorWrapper.appendChild(dcconListContainer);
-    dcconSelectorRoot.appendChild(dcconDialog);
     dcconSelectorRoot.appendChild(dcconSelectorWrapper);
-    
     isSelectorOpen = false;
   }
   dcconListContainer.innerHTML = "";
@@ -403,13 +427,76 @@ const inputAreaExists = () => {
 }
 
 
+const elementInitializer = () => {
+  function finder(){
+    if(isVod === false)
+    {
+      if(!inputArea)
+      {
+        logger(2 , "searching chat input area...");
+        inputArea = getChatInputArea();
+        if(inputArea)
+        {
+          inputAreaExists();
+        }
+      }
+      if(!iconArea)
+      {
+        logger(2 , "searching chat icon area...");
+        iconArea = getIconArea();
+        if(iconArea)
+        {
+
+        }
+      }
+    }
+
+    if(!rightColumn)
+    {
+      logger(2 , "searching right column...");
+      rightColumn = getRightColumn();
+      if(rightColumn)
+      {
+        dcconDialog = document.createElement('div');
+        dcconDialog.id = "dccon-dialog";
+        rightColumn.appendChild(dcconDialog);
+      }
+    }
+
+    if(!chatArea)
+    {
+      logger(2 , "searching chat area...");
+      chatArea = getChatArea();
+      if(chatArea)
+      { 
+        replaceChatAll();
+        chatObserver = new MutationObserver(chatObserverHandler);
+        chatObserver.observe(chatArea, chatObserverOptions);
+      }
+    }
+  }
+
+  finder();
+  const job = setInterval(() => {
+    if((isVod === false && (!inputArea || !iconArea)) || !chatArea || !rightColumn)
+    {
+      finder();
+    }
+    else 
+    {
+      clearInterval(job);
+    }
+  }, 100);
+}
+
+
 const run = async () => {
-  logger("loaded!");
+  logger(2 , "loaded!");
   // const streamer = getStreamerFromURL();
   const streamer = "funzinnu";
   if(!(streamer in URLS))
   {
-    logger(`${streamer} is not in whitelist. exit...`);
+    logger(2 , `${streamer} is not in whitelist. exit...`);
     return;
   }
 
@@ -421,32 +508,13 @@ const run = async () => {
       dccons = (await chrome.storage.local.get(streamer))[streamer].data;
     });
   }
-  logger(`loaded dccon length: ${dccons.length}`);
+  logger(2 , `loaded dccon length: ${dccons.length}`);
 
   titleArea = document.getElementsByTagName('title')[0];
   titleObserver = new MutationObserver(titleObserverHandler);
   titleObserver.observe(titleArea, titleObserverOptions);
 
-  while(!inputArea)
-  {
-    inputArea = getChatInputArea();
-    if(inputArea)
-    {
-      inputAreaExists();
-    }
-  }
-  while(!iconArea)
-  {
-    iconArea = getIconArea();
-  }
-  while(!chatArea)
-  {
-    chatArea = getChatArea();
-    replaceChatAll();
-
-    chatObserver = new MutationObserver(chatObserverHandler);
-    chatObserver.observe(chatArea, chatObserverOptions);
-  }
+  elementInitializer();
 }
 
 window.onload = run;
